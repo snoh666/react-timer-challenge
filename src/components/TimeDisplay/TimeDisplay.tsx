fcb0950a82bd75cc49d0ffc5b63164a25a294065
Snochowski Mikołaj <snochopl@gmail.com>
2020-12-13 21:49:55 +0100

    Add fontSize prop

--- src/components/TimeDisplay/TimeDisplay.tsx
+++ src/components/TimeDisplay/TimeDisplay.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 import TwoDigitNumber from '@components/TwoDigitNumber';
 
-const Paragraph = styled.p`
-  font-size: 0.75rem;
+interface ParagraphProps {
+  fontSize: string;
+}
+
+const Paragraph = styled.p<ParagraphProps>`
+  font-size: ${({ fontSize }) => fontSize};
 `;
 
 interface Props {
   time: number;
+  fontSize?: string;
 }
 
-const TimeDisplay: React.FC<Props> = ({ time }) => {
+const TimeDisplay: React.FC<Props> = ({ time, fontSize }) => {
   const ms = time % 100;
   const sec = Math.trunc(time / 100);
   const min = Math.trunc(time / 100 / 60);
   return (
-    <Paragraph>
+    <Paragraph fontSize={fontSize || '0.75rem'}>
       <TwoDigitNumber value={min} />
       <span>:</span>
       <TwoDigitNumber value={sec} />
