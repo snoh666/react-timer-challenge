b9f783dd18c8f07dd962972f2adbafca7ff9f95a
Snochowski Mikołaj <snochopl@gmail.com>
2020-12-13 22:50:53 +0100

    Update Basic styled wrapper

--- src/layouts/Basic/Basic.tsx
+++ src/layouts/Basic/Basic.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  padding: 2rem;
   width: 100%;
   min-height: 100vh;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: flex-start;
 `;
 
 const Basic: React.FC = ({ children }) => {
