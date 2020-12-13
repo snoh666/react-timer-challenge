4be3292e56d6012a2d353ccdf9845db806afbe30
Snochowski Mikołaj <snochopl@gmail.com>
2020-12-13 21:44:12 +0100

    Add Timestamp adding feature

--- src/components/Stopwatch/Stopwatch.tsx
+++ src/components/Stopwatch/Stopwatch.tsx
@@ -1,23 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { AddLap } from '@redux/laps/actions';
+import TimeDisplay from '@components/TimeDisplay';
 
 const Stopwatch: React.FC = () => {
   const [currentTime, setCurrentTime] = useState(0);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentTime(state => state + 1);
-    }, 1000);
+    }, 10);
 
     return () => {
       clearInterval(intervalId);
     };
   }, []);
 
+  const addTimestamp = () => {
+    dispatch(AddLap(currentTime));
+  };
+
   return (
     <div>
       <h2>Stopwatch component</h2>
       <p>Timer:</p>
-      <p>{currentTime}</p>
+      <div>
+        <TimeDisplay time={currentTime} />
+      </div>
+      <button type='button' onClick={() => addTimestamp()}>
+        Add Timestamp
+      </button>
     </div>
   );
 };
